94fc53404d464f2fc5461ac671b08b4578503a0b
Lý Thành Nhân <lythanhnhan27294@gmail.com>
2023-03-19 14:36:07 +0700

    fix: remove legacy nvchad 1.0 use neotree:

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,59 +1,59 @@
 local M = {}
 
 M.custom = {
-  i = {
-    ["ww"] = {
-      "<ESC>:w<CR>",
-      "write and escape",
-      opts = {
-        nowait = true,
-      },
-    },
-    ["jk"] = {
-      "<ESC>",
-      "escape insert mode",
-      opts = {
-        nowait = true,
-      },
-    },
-  },
-  n = {
+	i = {
+		["ww"] = {
+			"<ESC>:w<CR>",
+			"write and escape",
+			opts = {
+				nowait = true,
+			},
+		},
+		["jk"] = {
+			"<ESC>",
+			"escape insert mode",
+			opts = {
+				nowait = true,
+			},
+		},
+	},
+	n = {
 
-    [";"] = {
-      ":",
-      "enter cmdline",
-      opts = {
-        nowait = true,
-      },
-    },
-    ["<leader>j"] = { "<cmd> HopChar2 <CR>", "Hope auto jump" },
-    ["<leader>x"] = {
-      function()
-        require("nvchad_ui.tabufline").close_buffer()
-      end,
-      "close buffer",
-    },
-  },
+		[";"] = {
+			":",
+			"enter cmdline",
+			opts = {
+				nowait = true,
+			},
+		},
+		["<leader>j"] = { "<cmd> HopChar2 <CR>", "Hope auto jump" },
+		["<leader>x"] = {
+			function()
+				require("nvchad_ui.tabufline").close_buffer()
+			end,
+			"close buffer",
+		},
+	},
 }
 M.nvimtree = {
-  plugin = true,
-  n = {
-    ["<C-b>"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
-    ["<leader>j"] = { "<cmd> HopChar2 <CR>", "Hope auto jump" },
-  },
+	plugin = true,
+	n = {
+		["<C-b>"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
+		["<leader>j"] = { "<cmd> HopChar2 <CR>", "Hope auto jump" },
+	},
 }
 M.telescope = {
-  plugin = true,
-  i = {
-    ["<C-q>"] = {
-      function()
-        local actions = require "telescope.actions"
-        actions.smart_send_to_qflist()
-        actions.open_qflist()
-      end,
-      "send select file to quick and open",
-    },
-  },
+	plugin = true,
+	i = {
+		["<C-q>"] = {
+			function()
+				local actions = require("telescope.actions")
+				actions.smart_send_to_qflist()
+				actions.open_qflist()
+			end,
+			"send select file to quick and open",
+		},
+	},
 }
 
 return M
--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -1,75 +1,86 @@
-local overrides = require "custom.configs.overrides"
+local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = { -- Override plugin definition options
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  }, -- overrde plugin configs
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  }, -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
-  {
-    "utilyre/barbecue.nvim",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    config = function()
-      require("barbecue").setup()
-    end,
-  },
-  {
-    "lvimuser/lsp-inlayhints.nvim",
-    config = function()
-      require("lsp-inlayhints").setup()
-      vim.cmd [[hi LspInlayHint guifg=#5c6a72 guibg=NONE ]]
-    end,
-  },
-  { "rafamadriz/friendly-snippets" },
-  {
-    "mg979/vim-visual-multi",
-    branch = "master",
-  },
-  { "tpope/vim-surround", lazy = false },
-  {
-    "phaazon/hop.nvim",
-    lazy = false,
-    branch = "v2", -- optional but strongly recommended
-    config = function()
-      require("hop").setup {
-        keys = "etovxqpdygfblzhckisuran",
-      }
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { -- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	}, -- overrde plugin configs
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	}, -- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
+	{
+		"utilyre/barbecue.nvim",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		config = function()
+			require("barbecue").setup()
+		end,
+	},
+	{
+		"lvimuser/lsp-inlayhints.nvim",
+		config = function()
+			require("lsp-inlayhints").setup()
+			vim.cmd([[hi LspInlayHint guifg=#5c6a72 guibg=NONE ]])
+		end,
+	},
+	{ "rafamadriz/friendly-snippets" },
+	{
+		"mg979/vim-visual-multi",
+		branch = "master",
+	},
+	{ "tpope/vim-surround", lazy = false },
+	{
+		"phaazon/hop.nvim",
+		lazy = false,
+		branch = "v2", -- optional but strongly recommended
+		config = function()
+			require("hop").setup({
+				keys = "etovxqpdygfblzhckisuran",
+			})
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		config = require("custom.configs.neotree"),
+	},
 }
 
 return plugins
