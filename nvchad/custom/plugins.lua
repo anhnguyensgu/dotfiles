a8d780e9472d66caace317f90c109cbe0b7b6953
lynhan318 <lythanhnhan27294@gmail.com>
2023-05-04 10:17:32 +0700

    fix(dap): display ui on the left

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -198,7 +198,47 @@ local plugins = { -- Override plugin definition options
 		"rcarriga/nvim-dap-ui",
 		dependencies = "mfussenegger/nvim-dap",
 		config = function()
-			require("dapui").setup()
+			local opts = {
+				layouts = {
+					{
+						elements = {
+							{
+								id = "scopes",
+								size = 0.25,
+							},
+							{
+								id = "breakpoints",
+								size = 0.25,
+							},
+							{
+								id = "stacks",
+								size = 0.25,
+							},
+							-- {
+							-- 	id = "watches",
+							-- 	size = 0.25,
+							-- },
+						},
+						position = "right",
+						size = 40,
+					},
+					{
+						elements = {
+							-- {
+							-- 	id = "repl",
+							-- 	size = 0.5,
+							-- },
+							{
+								id = "console",
+								size = 0.5,
+							},
+						},
+						position = "bottom",
+						size = 10,
+					},
+				},
+			}
+			require("dapui").setup(opts)
 		end,
 	},
 }
